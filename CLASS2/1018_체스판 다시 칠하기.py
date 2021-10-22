70b37011620afeb6ab791ae811131d3be77e3d39
junseokcheon <goosw775@naver.com>
2021-10-22 08:46:46 +0000

    CLASS2

---
+++ CLASS2/1018_체스판 다시 칠하기.py
@@ -0,0 +1,31 @@
+# 문제의 핵심은 체스판 시작의 색깔이 흰색인 경우와 검은색인 경우이다.
+# 체스판은 흰색과 검은색 번갈아가며 체크무늬를 이뤄야 한다.
+# 보드판을 짤라서, 체크무늬가 되도록 색을 고치는 최소값을 찾는 것이다.
+
+N, M = map(int, input().split())
+board = []
+new = []
+
+for i in range(N):
+  board.append(input()) # board에 추가
+ 
+for i in range(N-7): # 8 * 8로 짤라야하기 때문에 행과 열을 N-7 / M-7로 고정시켜줘야 한다.
+  for j in range(M-7):
+    w = 0
+    b = 0
+    for k in range(i, i+8): # 체스판을 고정시킨다음 i,j부터 i+8,j+8까지 체크무늬를 확인한다.
+      for l in range(j, j+8):
+        if (k+l) % 2 == 0: # 현재 행 i와 열 j의 인덱스 합이 짝수이면 시작점의 색깔과 같아야 하고, 다르면 시작점의 색깔과 다르다는 점을 이용한다. 여기 if문은 시작점의 색깔과 같아야 하는데 다르다면 w(white), b(black)중에 고쳐야 할 곳에 count+1 해준다.
+          if board[k][l] != "W":
+            w+=1
+          if board[k][l] != "B":
+            b+=1
+        else: # else의 경우 인덱스 합이 홀수인 경우이다. 시작점의 색깔과 다를 때인데 다르지 않는 경우 count+1 해준다.
+          if board[k][l] != "W":
+            b+=1
+          if board[k][l] != "B":
+            w+=1
+    new.append(w)
+    new.append(b)
+
+print(min(new))
--- CLASS2/10989_수 정렬하기3.py
+++ CLASS2/10989_수 정렬하기3.py
@@ -12,3 +12,28 @@ for i in range(10001): # 처음부터 끝까지 for 반복
   if num_list[i] != 0: # 입력받은 수가 아니면 고려되지 않는 조건
     for j in range(num_list[i]): # 해당 숫자(= 입력받은 값이 몇 번 나왔는가)만큼 
       print(i) # 입력받은 값 출력
+
+
+# ex)
+# n = 6
+# m : 2 3 5 7 2 7
+# num_list : [ 0 0 2 1 0 1 0 2 0 0 0 0.... ]
+
+# i = 2, 3, 5, 7 일 때 고려됨
+# i = 2
+# for j in range(2)
+#   print(2) -> 2를 두번 출력
+
+# i = 3
+# for j in range(1)
+#   print(3) -> 3를 한번 출력
+
+# i = 5
+# for j in range(1)
+#   print(5) -> 5를 한번 출력
+
+# i = 7
+# for j in range(2)
+#   print(7) -> 7를 두 번 출력
+
+# --> 2 2 3 5 7 7 출력됨
